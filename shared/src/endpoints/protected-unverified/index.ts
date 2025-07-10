a3d4991479577ec6ce1aae40a1684d5515c9acb3
rlajustin <justin314kim@gmail.com>
2025-07-10 16:44:41 -0400

    fix

--- shared/src/endpoints/protected-unverified/index.ts
+++ shared/src/endpoints/protected-unverified/index.ts
@@ -1,9 +1,7 @@
-import { ProtectedEndpoint } from "src/types";
-
 import { ResendVerifyEmailEndpoint } from "./resend-verify-email";
 import { VerifyEmailEndpoint } from "./verify-email";
 
 export const ProtectedUnverifiedEndpoints = {
   RESEND_VERIFY_EMAIL: ResendVerifyEmailEndpoint,
   VERIFY_EMAIL: VerifyEmailEndpoint,
-} satisfies Record<string, ProtectedEndpoint>;
+};
--- shared/src/endpoints/protected/index.ts
+++ shared/src/endpoints/protected/index.ts
@@ -1,5 +1,3 @@
-import { ProtectedEndpoint } from "src/types";
-
 import { ExampleEndpoint } from "./example";
 import { DeleteAccountEndpoint } from "./delete-account";
 import { ChangeEmailRequestEndpoint } from "./change-email-request";
@@ -8,4 +6,4 @@ export const ProtectedEndpoints = {
   EXAMPLE: ExampleEndpoint,
   DELETE_ACCOUNT: DeleteAccountEndpoint,
   CHANGE_EMAIL_REQUEST: ChangeEmailRequestEndpoint,
-} satisfies Record<string, ProtectedEndpoint>;
+};
